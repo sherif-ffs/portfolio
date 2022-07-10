0c1b326b5b5adaa20a4eaf2c43ecb8e7ec939405
Sherif Elmetwally <sge5@njit.edu>
2022-07-10 16:31:20 -0400

    convert from template and other fixes

--- components/Nav.js
+++ components/Nav.js
@@ -1,27 +1,21 @@
-const NavigationTemplate = document.createElement('template');
-
-NavigationTemplate.innerHTML =
-  `
-  <link href="./components/nav.css" rel="stylesheet"></link>
-  <link href="../global.css" rel="stylesheet"></link>
-  <header>
-      <div class="border-two">
-        <div class="border-three">
-          <div class="border-four">
-            <div class="contact">
-              <a href="index.html">Home</a>
+class NavigationTemplate extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML =
+      `
+      <link href="./components/nav.css" rel="stylesheet"></link>
+      <header>
+          <div class="border-two">
+            <div class="border-three">
+              <div class="border-four">
+                <div class="contact">
+                  <a href="index.html">Home</a>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-  </header>
-`
-
-class Navigation extends HTMLElement {
-  connectedCallback() {
-    const shadowRoot = this.attachShadow({ mode: 'open' });
-    shadowRoot.appendChild(NavigationTemplate.content);
+      </header>
+    `;
   }
 }
 
-customElements.define('navigation-component', Navigation);
+customElements.define('navigation-component', NavigationTemplate);
