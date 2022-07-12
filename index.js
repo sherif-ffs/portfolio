fe3d7ab28d9afdccdaff1f340dc8e2d57f254605
Sherif Elmetwally <sge5@njit.edu>
2022-07-11 23:14:40 -0400

    persist themes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+console.log('Hello! Thank you for visiting my website :)')
+
 const root = document.documentElement;
 const doc = document.firstElementChild;
 
@@ -28,14 +30,44 @@ const hueInput = document.querySelector('hue-scroll-widget')
 const themeToggle = document.querySelector('theme-toggle');
 
 hueInput.addEventListener('input', (e) => {
+  localStorage.setItem('--brand-hue', e.target.value);
   root.style.setProperty('--brand-hue', e.target.value)
 })
 
 hueInput.addEventListener('onkeydown', (e) => {
+  localStorage.setItem('--brand-hue', e.target.value);
   root.style.setProperty('--brand-hue', e.target.value)
-
 })
 
 themeToggle.addEventListener('input', (e) => {
+  localStorage.setItem('color-scheme', e.target.value);
   doc.setAttribute('color-scheme', e.target.value)
 })
+
+
+const radioButtons = Array.from(document.querySelectorAll('.radio-input'))
+const slider = document.querySelector('.brand-hue-input');
+
+
+const renderChecked = (scheme) => {
+  radioButtons.forEach(button => {
+    const value = button.value;
+
+    if (value === scheme) {
+      button.checked = true
+    }
+  })
+}
+
+if (localStorage.getItem('color-scheme')) {
+  const theme = localStorage.getItem('color-scheme')
+  doc.setAttribute('color-scheme', theme)
+  renderChecked(theme)
+}
+
+
+if (localStorage.getItem('--brand-hue')) {
+  const hue = localStorage.getItem('--brand-hue')
+  root.style.setProperty('--brand-hue', hue)
+  slider.value = hue
+}
--- projects.js
+++ projects.js
@@ -8,6 +8,20 @@ const spirit = document.querySelector('.spirit');
 
 const width = window.innerWidth;
 
+const root = document.documentElement;
+const doc = document.firstElementChild;
+
+if (localStorage.getItem('color-scheme')) {
+  const theme = localStorage.getItem('color-scheme')
+  doc.setAttribute('color-scheme', theme)
+}
+
+
+if (localStorage.getItem('--brand-hue')) {
+  const hue = localStorage.getItem('--brand-hue')
+  root.style.setProperty('--brand-hue', hue)
+}
+
 const projects = [{
     name: 'garden',
     node: garden
@@ -40,27 +54,17 @@ const projects = [{
 const image = document.querySelector('.target-image');
 image.style.display = 'none'
 
-console.log({ width });
-
 if (width > 1000) {
   projects.forEach((project) => {
     project.node.addEventListener('mouseenter', (e) => {
-      console.log({ e })
       const top = e.layerY;
-      const left = e.x;
-      console.log({
-        top,
-        left
-      })
       document.querySelector('.dance').style.display = 'none';
       image.style.display = 'block';
       image.style.top = `${top-200}px`;
       image.style.zIndex = '10';
-      image.style.border = '1px solid red';
       image.src = `./images/${project.name}.png`
     })
     project.node.addEventListener('mouseleave', (e) => {
-      console.log({ e })
       image.style.display = 'none'
       document.querySelector('.dance').style.display = 'block';
       image.src = ""
