c39d0e98bfbe0d3652dba41cf9478527023cdba8
Sherif Elmetwally <sge5@njit.edu>
2022-07-11 21:47:59 -0400

    photo stuff

--- projects.js
+++ projects.js
@@ -4,6 +4,7 @@ const hoard = document.querySelector('.hoard');
 const neolib = document.querySelector('.neolib');
 const test = document.querySelector('.test');
 const wealth = document.querySelector('.wealth');
+const spirit = document.querySelector('.spirit');
 
 const width = window.innerWidth;
 
@@ -30,6 +31,10 @@ const projects = [{
   {
     name: 'wealth',
     node: wealth
+  },
+  {
+    name: 'spirit',
+    node: spirit
   }
 ]
 const image = document.querySelector('.target-image');
@@ -41,8 +46,17 @@ if (width > 1000) {
   projects.forEach((project) => {
     project.node.addEventListener('mouseenter', (e) => {
       console.log({ e })
+      const top = e.layerY;
+      const left = e.x;
+      console.log({
+        top,
+        left
+      })
       document.querySelector('.dance').style.display = 'none';
-      image.style.display = 'block'
+      image.style.display = 'block';
+      image.style.top = `${top-200}px`;
+      image.style.zIndex = '10';
+      image.style.border = '1px solid red';
       image.src = `./images/${project.name}.png`
     })
     project.node.addEventListener('mouseleave', (e) => {
